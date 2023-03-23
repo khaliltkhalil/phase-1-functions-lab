e34e605363e2f8432562eaa83339baacc95a3114
Khalil Khalil <khalil.t.khalil@live.com>
2023-03-23 11:19:38 -0500

    writes index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const hqLocation = 42;
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(pickup - hqLocation);
+}
+
+function distanceFromHqInFeet(pickup) {
+    return 264 * distanceFromHqInBlocks(pickup);
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(endBlock - startBlock) * 264;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    const distInFeet = distanceTravelledInFeet(startBlock, endBlock);
+    if(distInFeet < 400) return 0;
+    if(distInFeet >= 400 && distInFeet <= 2000) {
+        return (distInFeet - 400) * 0.02;
+    }
+    if(distInFeet > 2000 && distInFeet < 2500) {
+        return 25;
+    }
+    return 'cannot travel that far';
+}
